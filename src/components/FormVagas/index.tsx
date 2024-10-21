6f4752b7551be21fb70ac787c982d4559b1aecbc
leonardo Nunes <leonardo.nunes1194@gmail.com>
2024-10-21 17:11:11 -0300

    test 4

---
+++ src/components/FormVagas/index.tsx
@@ -0,0 +1,28 @@
+import { FormEvent, useState } from 'react'
+
+import { Form, Campo, Button } from './styles'
+
+type Props = {
+  aoPesquisar: (termo: string) => void
+}
+
+const FormVagas = ({ aoPesquisar }: Props) => {
+  const [termo, setTermo] = useState<string>('')
+
+  const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    aoPesquisar(termo.toLocaleLowerCase())
+  }
+
+  return (
+    <Form onSubmit={aoEnviarForm}>
+      <Campo
+        placeholder="Front-end, fullstack, node, design"
+        onChange={(e) => setTermo(e.target.value)}
+        type="search"
+      />
+      <Button type="submit">Pesquisar</Button>
+    </Form>
+  )
+}
+export default FormVagas
